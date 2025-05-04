43834c3de31024d70381c3747299943a6494cb3c
sakurayoru <hiroseshun2000@gmail.com>
2025-05-04 09:21:10 +0900

    [add]for Releases.zip bat

---
+++ copy-file.bat
@@ -0,0 +1,4 @@
+@echo off
+copy "00-config-sample.json" "CheckDriveSpace\bin\Release\00-config.json"
+copy "README.md" "CheckDriveSpace\bin\Release\00-README.md"
+copy "LICENSE.txt" "CheckDriveSpace\bin\Release\00-LICENSE.txt"
